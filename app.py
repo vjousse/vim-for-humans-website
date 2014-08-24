9ccea6222a18e475b203f5d8d3363363395fa23d
Vincent Jousse <vincent@jousse.org>
2014-08-24 22:53:33 +0200

    Add payment download page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 import stripe
 
@@ -22,14 +22,17 @@ def index():
 def download():
     return render_template('download.html', key=stripe_keys['publishable_key'])
 
+@app.route('/confirm/<int:amount>')
+def confirm(amount):
+    return render_template('confirm.html', amount=amount/100)
+
 @app.route('/charge', methods=['POST'])
 def charge():
     #{'id': 'tok_14TwY1KRgapzZVSgDU5fI3Io', 'livemode': False, 'email': 'vincent@jousse.org', 'card': {'id': 'card_14TwY1KRgapzZVSgD4JHoVDa', 'brand': 'Visa', 'address_zip': None, 'exp_month': 12, 'address_city': None, 'name': 'vincent@jousse.org', 'customer': None, 'country': 'US', 'address_state': None, 'fingerprint': 'KNmudQJYDGikF2dE', 'address_line2': None, 'last4': '4242', 'exp_year': 2023, 'address_line1': None, 'funding': 'credit', 'address_country': None, 'object': 'card'}, 'type': 'card', 'created': 1408631297, 'used': False, 'object': 'token'}
 
     # Amount in cents
     amount = float(request.form['amount'])
-    amountInCents = int(amount)*100
-    print("Charging {}".format(amount))
+    amountInCents = int(amount*100)
 
     if(amount != 0.0):
         customer = stripe.Customer.create(
@@ -44,9 +47,9 @@ def charge():
             description='Vim pour les humains'
         )
 
-    return render_template('charge.html', amount=amount)
+    return redirect(url_for('confirm', amount=amountInCents))
 
-class Purchase(db.Model):
+class Download(db.Model):
     __tablename__ = 'purchase'
     uuid = db.Column(db.String, primary_key=True)
     email = db.Column(db.String)
