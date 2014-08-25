1fceb678008f44cd646b12e142b6d3e004a8f821
Vincent Jousse <vincent@jousse.org>
2014-08-25 09:35:21 +0200

    Create db

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-Run with:
+# Installation
+
+## Create db
+
+    PUBLISHABLE_KEY=pk_test_4aZnwTrOtTfNSxs8KtI1a3LC SECRET_KEY=sk_test_4aZnq9QgKUcJ2cPp9dGaidOT python
+    from app import tdb
+    db.create_all()
+
+## Run
 
     PUBLISHABLE_KEY=pk_test_4aZnwTrOtTfNSxs8KtI1a3LC SECRET_KEY=sk_test_4aZnq9QgKUcJ2cPp9dGaidOT python app.py
 
-Good reads:
+
+# Good reads
 
 - https://eff.iciently.com/blog/stripe_checkout.html
 - http://www.jeffknupp.com/blog/2014/01/18/python-and-flask-are-ridiculously-powerful/
--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template, request, url_for, redirect, send_from_
 from flask.ext.sqlalchemy import SQLAlchemy
 import stripe
 
+app = Flask(__name__, static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vimebook.db'
+db = SQLAlchemy(app)
+
 stripe_keys = {
     'secret_key': os.environ['SECRET_KEY'],
     'publishable_key': os.environ['PUBLISHABLE_KEY']
 }
 
 stripe.api_key = stripe_keys['secret_key']
-
-app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vimebook.db'
-db = SQLAlchemy(app)
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
@@ -33,7 +33,6 @@ def confirm(amount):
 
 @app.route('/charge', methods=['POST'])
 def charge():
-    #{'id': 'tok_14TwY1KRgapzZVSgDU5fI3Io', 'livemode': False, 'email': 'vincent@jousse.org', 'card': {'id': 'card_14TwY1KRgapzZVSgD4JHoVDa', 'brand': 'Visa', 'address_zip': None, 'exp_month': 12, 'address_city': None, 'name': 'vincent@jousse.org', 'customer': None, 'country': 'US', 'address_state': None, 'fingerprint': 'KNmudQJYDGikF2dE', 'address_line2': None, 'last4': '4242', 'exp_year': 2023, 'address_line1': None, 'funding': 'credit', 'address_country': None, 'object': 'card'}, 'type': 'card', 'created': 1408631297, 'used': False, 'object': 'token'}
 
     # Amount in cents
     amount = float(request.form['amount'])
@@ -55,9 +54,11 @@ def charge():
     return redirect(url_for('confirm', amount=amountInCents))
 
 class Download(db.Model):
-    __tablename__ = 'purchase'
+    __tablename__ = 'download'
     uuid = db.Column(db.String, primary_key=True)
     email = db.Column(db.String)
+    price = db.Column(db.Float, default=0)
+    count = db.Column(db.Integer, default=0)
 
 
 if __name__ == '__main__':
