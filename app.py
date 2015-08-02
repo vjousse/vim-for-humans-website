f742cbee12a5141bb15a9f78f0f4a1222a5009be
Vincent Jousse <vincent@jousse.org>
2015-08-02 10:03:43 +0200

    Add new design to the Flask app

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def before():
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
+@app.route('/favicon.ico')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
