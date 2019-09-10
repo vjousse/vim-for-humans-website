a729c11cae97fb444d16fc6c9f98625b18e36334
Vincent Jousse <vincent@jousse.org>
2019-09-10 17:05:41 +0200

    Update endpoints for new stripe API

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 babel = Babel(app, default_locale='fr')
 
 stripe.api_key = STRIPE_KEYS['secret_key']
+endpoint_secret = STRIPE_KEYS['endpoint_secret']
 
 
 @babel.localeselector
@@ -107,7 +108,9 @@ def download_file(download_uuid, name, format):
 
 @app.route('/<lang_code>/confirm/<download_uuid>')
 def confirm(download_uuid):
+
     download = Download.query.get(download_uuid)
+
     return render_template(
         'confirm.html', amount=download.price, uuid=download_uuid)
 
@@ -122,45 +125,83 @@ def cancel():
     return render_template('cancel.html')
 
 
+@app.route('/stripe_webhook', methods=['POST'])
+def stripe_webhook():
+    payload = request.data
+
+    sig_header = request.headers.get('STRIPE_SIGNATURE')
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+                )
+    except ValueError as e:
+        print(e)
+        # Invalid payload
+        abort(400)
+    except stripe.error.SignatureVerificationError as e:
+        print(e)
+        # Invalid signature
+        abort(400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        # handle_checkout_session(session)
+        print(session)
+
+        download = Download(
+            uuid=session['client_reference_id'],
+            email=session['customer_email'],
+            price=session['display_items'][0]['amount']/100,
+            lang=g.get('current_lang'))
+
+        db.session.add(download)
+        db.session.commit()
+
+    return ('', 200)
+
+
 @app.route('/<lang_code>/charge', methods=['POST'])
 def charge():
 
     # Amount in cents
     amount = float(request.form['amount'])
+    amountInCents = int(amount*100)
     download_uuid = str(uuid.uuid4())
+
+    print(url_for(
+            'cancel',
+            lang_code=g.get('current_lang', 'fr')))
 
     if(amount != 0.0):
 
-        stripe.checkout.Session.create(
+        session = stripe.checkout.Session.create(
+          client_reference_id=download_uuid,
           payment_method_types=['card'],
           line_items=[{
             'name': 'Vim for humans',
-            'amount': amount,
+            'amount': amountInCents,
             'currency': 'eur',
             'quantity': 1,
           }],
           success_url=url_for(
             'confirm',
             download_uuid=download_uuid,
-            lang_code=g.get('current_lang', 'fr')),
+            lang_code=g.get('current_lang', 'fr'),
+            _external=True),
           cancel_url=url_for(
             'cancel',
-            lang_code=g.get('current_lang', 'fr'))
+            lang_code=g.get('current_lang', 'fr'),
+            _external=True)
         )
 
-        download = Download(
-                uuid=download_uuid,
-                email=request.form['email'],
-                price=amount,
-                lang=g.get('current_lang'))
-
-        db.session.add(download)
-        db.session.commit()
-
-        return redirect(url_for(
-            'confirm',
-            download_uuid=download_uuid,
-            lang_code=g.get('current_lang', 'fr')))
+        return render_template(
+            'charge.html',
+            key=STRIPE_KEYS['publishable_key'],
+            checkout_session_id=session.id)
 
     else:
 
