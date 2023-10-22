a95b04599dae24b3b9dd288c346ab51df0377d2c
Vincent Jousse <vincent@jousse.org>
2023-10-22 22:50:42 +0200

    Remove useless activate_this.py

--- wsgi_prod.py
+++ wsgi_prod.py
@@ -1,10 +1,7 @@
-import os, sys
+import os
+import sys
 
-PROJECT_DIR = '/home/vimebook/python/vim-for-humans-website'
-
-activate_this = os.path.join(PROJECT_DIR, 'env', 'bin', 'activate_this.py')
-#execfile(activate_this, dict(__file__=activate_this))
-exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'), dict(__file__=activate_this))
+PROJECT_DIR = "/home/vimebook/python/vim-for-humans-website"
 sys.path.append(PROJECT_DIR)
 
 from app import app as application
