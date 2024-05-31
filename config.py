ea40b6f2f088ebf6f0125a8cadcc8146ea19254b
Vincent Jousse <vincent@jousse.org>
2024-05-31 15:32:49 +0200

    Update for english version

--- config.py
+++ config.py
@@ -2,21 +2,15 @@ import os
 
 # -*- coding: utf-8 -*-
 # available languages
-LANGUAGES = {
-    'fr': 'Français',
-    'en': 'English'
-}
+LANGUAGES = {"fr": "Français", "en": "English"}
 
 STRIPE_KEYS = {
-    'secret_key': os.environ['SECRET_KEY'],
-    'publishable_key': os.environ['PUBLISHABLE_KEY'],
-    'endpoint_secret': os.environ['ENDPOINT_SECRET']
+    "secret_key": os.environ["SECRET_KEY"],
+    "publishable_key": os.environ["PUBLISHABLE_KEY"],
+    "endpoint_secret": os.environ["ENDPOINT_SECRET"],
 }
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///vimebook.db'
+SQLALCHEMY_DATABASE_URI = "sqlite:///vimebook.db"
 
-UPDATES = {
-    'fr': '25/05/2024',
-    'en': None
-}
+UPDATES = {"fr": "25/05/2024", "en": "2024/05/31"}
